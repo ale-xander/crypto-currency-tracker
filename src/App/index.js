4f147f9a9018fd6d27753fa8a694721ed88697bd
alexiliev <iliev.alex@outlook.com>
2020-03-30 18:22:20 -0700

    setting up the basic file structure and settings

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import styled, {css} from 'styled-components';
+
+
+function App() {
+  return (
+  <>
+    hello world.
+    
+  </>
+  );
+}
+
+export default App;
