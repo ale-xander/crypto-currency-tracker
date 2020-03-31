c7986d4ed847abee7cda528bad57e8a6a03de408
alexiliev <iliev.alex@outlook.com>
2020-03-30 19:52:32 -0700

    added styling to navbar

--- src/App/NavBar.jsx
+++ src/App/NavBar.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const NavBar = styled.div`
     display:grid;
     grid-template-columns: 100px auto 100px 100px;
-`
+    margin-bottom: 40px;
+`;
+
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1)
+};
+
+const Logo = styled.div`
+    font-size: 2em;
+`;
+
+const ControlButtonElement = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+        text-shadow: 0px 0px 100px #55ffbb; 
+    `}
+`;
+// a functional component that's a wrapper for the buttons
+function ControlButton({name, active}) {
+    return(
+        <ControlButtonElement active={active}>
+            {toProperCase(name)}
+        </ControlButtonElement>
+    )
+};
 
 export default function () {
-    return <NavBar>
-        <div>Logo_here</div>
-        <div/>
-        <div>Dashboard</div>
-        <div>Settigns</div>
-      
+    return (<
+        NavBar>
+            <Logo>Logo_here</Logo>
+            <div/>
+            <ControlButton active name="dashboard"/>
+            <ControlButton active name="Settings"/>
         
         </NavBar>
+    )
 }
