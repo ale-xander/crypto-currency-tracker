2bae9b1b7e016499cc0f1e193515f44f0982590c
alexiliev <iliev.alex@outlook.com>
2020-04-01 15:33:28 -0700

    created a grid element to display allcoins in separate tiles

--- src/Settings/CoinGrid.jsx
+++ src/Settings/CoinGrid.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import {AppContext} from "../App/AppProvider";
+import {SelectableTile} from '../Shared/Tile'
 
 export const CoinGridStyled = styled.div`
   display: grid;   
-  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr)); 
-  
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+  grid-gap: 15px; 
+  margin-top: 40px; 
 `
 
 export default function (){
@@ -13,7 +15,9 @@ export default function (){
     <AppContext.Consumer>
     {({coinList}) => (
       <CoinGridStyled>
-            {Object.keys(coinList).map(coinKey => <>{coinKey}</>)}
+            {Object.keys(coinList).map(coinKey => 
+            <SelectableTile>{coinKey}</SelectableTile>
+            )}
         
       </CoinGridStyled>
     )}
