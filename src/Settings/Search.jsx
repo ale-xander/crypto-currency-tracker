5b344e219a894e79ee6defad3f4ff73ec878c7ac
alexiliev <iliev.alex@outlook.com>
2020-04-10 14:07:42 -0700

    styling the search component

--- src/Settings/Search.jsx
+++ src/Settings/Search.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import {backgroundColor2, fontSize2} from "../Shared/Styles";
+
+
+const SearchGrid = styled.div`
+    display: grid; 
+    grid-template-columns: 200px 1fr; 
+`
+const SearchInput = styled.input`
+    
+    ${fontSize2}
+    border: 1px solid; 
+    height: 25px; 
+    color: black;
+    place-self: center left; 
+`
 
 export default function () {
-    return <> SEARCH </>
+    return (
+        <SearchGrid> 
+            saerch all coins 
+            <SearchInput />
+        </SearchGrid>
+    )
 }
