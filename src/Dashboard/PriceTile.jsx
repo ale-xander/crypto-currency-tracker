a5ddd4b799b1ce5436cda536033c8b60bb88b691
alexiliev <iliev.alex@outlook.com>
2020-04-10 21:10:57 -0700

    styling the price tile

--- src/Dashboard/PriceTile.jsx
+++ src/Dashboard/PriceTile.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {SelectableTile} from "../Shared/Tile";
+import {fontSize3, fontSizeBig, greenBoxShadow} from "../Shared/Styles";
+import {CoinHeaderGridStyled} from '../Settings/CoinHeaderGrid'
 
+const numberFormat = number => {
+    return +(number + '').slice(0, 6);
+}
 
+const PriceTileStyled = styled(SelectableTile)`
+    ${props => props.compact && css`
+        display: grid; 
+        ${fontSize3}
+        grid-gap: 5px; 
+        grid-template-columns: repeat(3, 1fr); 
+        justify-items: right; 
+    `}
+    
+    ${props => props.currentFavorite && css`
+        ${greenBoxShadow}
+        pointer-events: none; 
+    `}
+`
+function PriceTile({sym, data}){
+    return (
+        <PriceTileStyled>
+            <CoinHeaderGridStyled>
+                <div> {sym} </div>
+                <div>{numberFormat(data.CHANGEPCT24HOUR)}</div>
+            </CoinHeaderGridStyled>
+        </PriceTileStyled>
+    );
+}
 
 export default function ({price, index}) {
     
@@ -10,9 +39,9 @@ export default function ({price, index}) {
     let data = price[sym]['USD'];
 
     return(
-        <SelectableTile>
+        <PriceTile sym={sym} data={data} >
             {sym}
             {data.PRICE}
-        </SelectableTile>
+        </PriceTile>
     )
 }
