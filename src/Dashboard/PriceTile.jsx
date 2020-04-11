37f5f252667d2580e5e1009d74181e67a570f0a3
alexiliev <iliev.alex@outlook.com>
2020-04-10 21:57:16 -0700

    fixed more styling on the price tile component

--- src/Dashboard/PriceTile.jsx
+++ src/Dashboard/PriceTile.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 import {SelectableTile} from "../Shared/Tile";
 import {fontSize3, fontSizeBig, greenBoxShadow} from "../Shared/Styles";
-import {CoinHeaderGridStyled} from '../Settings/CoinHeaderGrid'
+import {CoinHeaderGridStyled} from "../Settings/CoinHeaderGrid";
 
-const numberFormat = number => {
-    return +(number + '').slice(0, 6);
-}
 const JustifyRight = styled.div`
     justify-self: right; 
 `
+
 const JustifyLeft = styled.div`
     justify-self: left; 
 `
+
 const TickerPrice = styled.div`
     ${fontSizeBig};
 `
+
 const ChangePct = styled.div`
     color: green; 
     ${props => props.red && css`
         color: red; 
     `}
 `
+
+const numberFormat = number => {
+    return +(number + '').slice(0, 6);
+}
 
 const PriceTileStyled = styled(SelectableTile)`
     ${props => props.compact && css`
@@ -37,38 +41,51 @@ const PriceTileStyled = styled(SelectableTile)`
         pointer-events: none; 
     `}
 `
-function PriceTile({sym, data}){
-    return (
-        <PriceTileStyled>
-            <CoinHeaderGridStyled>
-                <div> {sym} </div>
-                <ChangePercent data={data} />
-            </CoinHeaderGridStyled>
-            <TickerPrice>
-                ${numberFormat(data.PRICE)}
-            </TickerPrice>
-        </PriceTileStyled>
-    );
-}
 
 function ChangePercent({data}){
     return (
         <JustifyRight>
-            <ChangePct red={data.CHANGEPCT24HOUR < 0}>
+        <ChangePct red={data.CHANGEPCT24HOUR < 0}>
             {numberFormat(data.CHANGEPCT24HOUR)}%
-            </ChangePct>
+        </ChangePct>
         </JustifyRight>
     );
 }
 
-export default function ({price, index}) {
-    
+function PriceTile({sym, data}){
+    return (
+        <PriceTileStyled >
+        <CoinHeaderGridStyled>
+            <> {sym} </>
+            <ChangePercent data={data}/>
+        </CoinHeaderGridStyled>
+        <TickerPrice>
+            ${numberFormat(data.PRICE)}
+        </TickerPrice>
+        </PriceTileStyled>
+    );
+}
+
+function PriceTileCompact({sym, data}){
+  return (
+    <PriceTileStyled >
+      <JustifyLeft> {sym} </JustifyLeft>
+      <ChangePercent data={data}/>
+      <>
+        ${numberFormat(data.PRICE)}
+      </>
+    </PriceTileStyled>
+  );
+}
+
+export default function({price, index}){
     let sym = Object.keys(price)[0];
     let data = price[sym]['USD'];
-
-    return(
-        <PriceTile sym={sym} data={data} >
-        
-        </PriceTile>
+    let TileClass = index < 5 
+        ? PriceTile 
+        : PriceTileCompact;
+    return (
+            <TileClass sym={sym} data={data}>
+            </TileClass>
     )
 }
