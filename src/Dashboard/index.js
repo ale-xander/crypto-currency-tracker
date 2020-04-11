6eb03a766599d0c3dce5f483b892b01fa0f1df5f
alexiliev <iliev.alex@outlook.com>
2020-04-10 17:22:53 -0700

    adding a component to track the prices of coins

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Page from '../Shared/Page';
-
+import PriceGrid from './PriceGrid'
 
 export default function(){
     return(
         <Page name="dashboard">
-            Dashboard be here at some point
+            <PriceGrid />
         </Page>
     )
 }
