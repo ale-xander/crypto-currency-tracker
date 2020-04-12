5f6503fd7e1ecbfdb471ff937fa7a44939a34178
alexiliev <iliev.alex@outlook.com>
2020-04-12 16:31:22 -0700

    styling the chart

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Page from '../Shared/Page';
 import PriceGrid from './PriceGrid';
 import CoinSpotlight from './CoinSpotlight'
+import styled from 'styled-components';
+
+const ChartGrid = styled.div`
+    display: grid; 
+    margin-top: 20px; 
+    grid-gap: 15px; 
+    grid-template-columns: 1fr 3fr; 
+`
 
 export default function(){
     return(
         <Page name="dashboard">
             <PriceGrid />
-            <CoinSpotlight />
+            <ChartGrid>
+                <CoinSpotlight />
+            </ChartGrid>
         </Page>
     )
 }
