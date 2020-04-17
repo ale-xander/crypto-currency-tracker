72a9748ab8abe764c8cb034bae281beb4badf702
alexiliev <iliev.alex@outlook.com>
2020-04-16 21:13:30 -0700

    importing the testing libs

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import './index.js';
+import App from './index.js';
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
 test('renders learn react link', () => {
-  
+  const wrapper = shallow(<App />)
+  //console.log(wrapper.debug())
+  expect(wrapper).toBeTruthy()
 });
